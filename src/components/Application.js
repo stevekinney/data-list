dfb58ca8b2726244e6d4be9884211704e3966caa
Steve Kinney <hello@stevekinney.net>
2017-12-02 15:31:13 -0700

    Add the filter box and just don't do anything

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,26 +2,63 @@ import React, { Component } from 'react';
 
 import data from '../data.json';
 
+class DataGrid extends Component {
+  shouldComponentUpdate(nextState, nextProps) {
+    return false;
+  }
+
+  render() {
+    return (
+      <table>
+        <thead />
+        <tbody>
+          {this.props.data.map((datum) => (
+            <tr>
+              {Object.keys(datum).map((column) => (
+                <td>{datum[column]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class Filter extends Component {
+  handleChange = event => {
+    this.props.onChange(event.target.value);
+  };
+
+  render() {
+    return (
+      <section className="Filter">
+        <input
+          type="text"
+          value={this.props.filterText}
+          onChange={this.handleChange}
+        />
+      </section>
+    );
+  }
+}
+
 export default class Application extends Component {
-  state = { data };
+  state = {
+    data: data.slice(0, 10),
+    filterText: '',
+  };
+
+  updateFilterText = (filterText) => {
+    this.setState({ filterText });
+  }
 
   render() {
     return (
       <div className="Application">
         <h1>Company Directory</h1>
-        <table>
-          <thead />
-          <tbody>
-            {this.state.data.map(datum => (
-              <tr>
-                {Object.keys(datum).map(column => (
-                  <td>{datum[column]}</td>
-                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
+        <Filter onChange={this.updateFilterText} />
+        <DataGrid data={this.state.data} />
       </div>
     );
   }
