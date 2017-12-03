483a85e114dd817f9ae09b1a01132f8dfe850fda
Steve Kinney <hello@stevekinney.net>
2017-12-03 08:22:09 -0700

    Add stuff

--- bin/generate-data.js
+++ bin/generate-data.js
@@ -2,20 +2,30 @@ const fs = require('fs');
 const path = require('path');
 const faker = require('faker');
 const uniqueId = require('lodash/uniqueId');
+const sample = require('lodash/sample');
 
 const data = [];
+
+const transgressions = [
+  'Ordered lobster and then suggested we split the bill',
+  'Parked too close to me',
+  'Did not brew another put of coffee',
+  'Forgot to wish me a happy birthday',
+  'Didn\'t watch my Frontend Masters course',
+];
+
+const getForgivenessStatus = () => {
+  if (Math.random() > 0.9) return true;
+  return false;
+};
 
 for (let i = 0; i < 1000; i++) {
   const item = {
     id: uniqueId(),
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    company: faker.company.companyName(),
-    department: faker.commerce.department(),
-    city: faker.address.city(),
-    state: faker.address.state(),
-    zip: faker.address.zipCode(),
-    date: faker.date.recent(),
+    fullName: faker.name.findName(),
+    transgression: sample(transgressions),
+    dateAdded: faker.date.past(4),
+    forgiven: getForgivenessStatus(),
   };
 
   data.push(item);
--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
+import moment from 'moment';
+import orderBy from 'lodash/orderBy';
 
 import data from '../data.json';
 
-class DataGrid extends Component {
-  shouldComponentUpdate(nextState, nextProps) {
-    return false;
-  }
+class NewGrudge extends PureComponent {
+  state = {
+    fullName: '',
+    transgression: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { onSubmit } = this.props;
+    const { fullName, transgression } = this.state;
+
+    onSubmit({
+      id: Date.now(),
+      fullName,
+      transgression,
+      forgiven: false,
+      dateAdded: new Date(),
+    });
+
+    this.setState({
+      fullName: '',
+      transgression: '',
+    });
+  };
 
   render() {
+    const { fullName, transgression } = this.state;
+    return (
+      <form className="NewGrudge" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={fullName}
+          name="fullName"
+          placeholder="Full Legal Name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          value={transgression}
+          name="transgression"
+          placeholder="What did they do?"
+          onChange={this.handleChange}
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+class DataRow extends PureComponent {
+  render() {
+    const { datum, onForgive } = this.props;
+    return (
+      <tr>
+        <td>{datum.fullName}</td>
+        <td>{datum.transgression}</td>
+        <td>{moment(datum.dateAdded).format('MMMM Do YYYY')}</td>
+        <td>{datum.forgiven ? 'Forgiven' : 'Not Forgiven'}</td>
+        <td>
+          <button onClick={() => onForgive(datum.id)}>
+            {datum.forgiven ? 'Unforgive' : 'Forgive'}
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+class DataGrid extends PureComponent {
+  filterData = datum =>
+    datum.transgression
+      .toLowerCase()
+      .includes(this.props.filterText.toLowerCase());
+
+  renderDataRow = (datum, index) => {
+    const { onForgive } = this.props;
+    return <DataRow key={index} datum={datum} onForgive={onForgive} />;
+  };
+
+  render() {
+    const { data, filterText, onForgive, onSort } = this.props;
+    const filteredData = data.filter(this.filterData);
+
     return (
       <table>
-        <thead />
-        <tbody>
-          {this.props.data.map((datum) => (
-            <tr>
-              {Object.keys(datum).map((column) => (
-                <td>{datum[column]}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
+        <thead>
+          <tr>
+            <th onClick={() => onSort('fullName')}>Full Legal Name</th>
+            <th onClick={() => onSort('transgression')}>Transgression</th>
+            <th onClick={() => onSort('dateAdded')}>Date of Incident</th>
+            <th onClick={() => onSort('forgiven')}>Forgiven?</th>
+            <th><button onClick={() => onSort('')} >Reset Sort</button></th>
+          </tr>
+        </thead>
+        <tbody>{orderBy(filteredData, [this.props.sortBy]).map(this.renderDataRow)}</tbody>
       </table>
     );
   }
@@ -45,20 +129,52 @@ class Filter extends Component {
 
 export default class Application extends Component {
   state = {
-    data: data.slice(0, 10),
+    data,
     filterText: '',
+    sortBy: '',
   };
 
-  updateFilterText = (filterText) => {
+  addGrudge = datum => {
+    const { data } = this.state;
+    this.setState({
+      data: [...data, datum],
+    });
+  };
+
+  forgiveGrudge = id => {
+    performance.mark('start');
+    const data = this.state.data.map(datum => {
+      if (id !== datum.id) return datum;
+      return { ...datum, forgiven: !datum.forgiven };
+    });
+    this.setState({ data });
+    performance.mark('end');
+    performance.measure('forgiveness', 'start', 'end');
+    const [measure] = performance.getEntriesByName('forgiveness');
+    console.log(measure);
+  };
+
+  updateFilterText = filterText => {
     this.setState({ filterText });
-  }
+  };
+
+  updateSortColumn = sortBy => {
+    this.setState({ sortBy });
+  };
 
   render() {
     return (
       <div className="Application">
         <h1>Company Directory</h1>
+        <NewGrudge onSubmit={this.addGrudge} />
         <Filter onChange={this.updateFilterText} />
-        <DataGrid data={this.state.data} />
+        <DataGrid
+          data={this.state.data}
+          filterText={this.state.filterText}
+          onForgive={this.forgiveGrudge}
+          onSort={this.updateSortColumn}
+          sortBy={this.state.sortBy}
+        />
       </div>
     );
   }
